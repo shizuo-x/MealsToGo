8b78790cc2c4ed1936702ebe683e227b862e463e
Shizuo-x <shizuo@local.pc>
2022-08-12 09:06:20 +0530

    after navbar v1

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 /* eslint-disable react/react-in-jsx-scope */
 import * as React from "react";
+import { Text, View } from 'react-native';
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen.js";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme/index.js";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 
+function HomeScreen() {
+  return (
+    <ThemeProvider theme={theme}>
+      <RestaurantsScreen />
+    </ThemeProvider>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Settings" component={SettingsScreen} options={{ headerShown: true }} />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
+
   const [latoLoaded] = useLato({
     Lato_400Regular,
   });
@@ -24,10 +56,10 @@ export default function App() {
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
-      </ThemeProvider>
       <ExpoStatusBar style="auto" />
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
     </>
   );
 }
